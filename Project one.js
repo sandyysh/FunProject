6cab8318067a0b9ca82d0f0ca24da15e2e78cc0e
sandyyeo93 <126796964+sandyyeo93@users.noreply.github.com>
2023-04-02 22:35:10 +0800

    created obstacles every 2 secs

--- Project one.js
+++ Project one.js
@@ -13,6 +13,7 @@ $(() => {
 
   const walkingBall = document.querySelector('.walkingBall'); 
   const jumpButton = document.querySelector('button');
+  const grid = document.querySelector('.grid')
 
   function control(e) {
     if (e.keyCode === 32) {
@@ -23,20 +24,20 @@ $(() => {
   jumpButton.addEventListener('click',jump);
   document.addEventListener('keyup', control);
   
-let isJumping = false; 
+  let isJumping = false; 
 
   function jump() {
     if (isJumping) return; 
     isJumping = true;
     
-    let position = 300;
+    let position = 400;
     let timerId = setInterval(function() {
       //move up 
       console.log('up');
       position += 30;
       walkingBall.style.bottom = position + 'px';
 
-      if (position >= 500) {
+      if (position >= 600) {
         clearInterval(timerId);
         fall();
       }
@@ -44,17 +45,74 @@ let isJumping = false;
   }     
 
   function fall() {
-    let position = 500;
+    let position = 600;
     let timerId = setInterval(function() {
       //move down 
       console.log('down');
       position -= 30;
       walkingBall.style.bottom = position + 'px';
 
-      if (position <= 300) {
+      if (position <= 400) {
         clearInterval(timerId);
         isJumping = false; //reset isJumping to false
       } 
     }, 20);
   }
+  let obstaclePosition = 0;
+  
+  function generateObstacles(){
+    let randomTime = Math.random() * 4000; 
+   
+ 
+    obstacle = document.createElement('div');
+    obstacle.classList.add('obstacle');
+    grid.appendChild(obstacle)
+    obstacle.style.right = obstaclePosition + 'px';
+    
+  }
+
+  // let timerID = setInterval(function() {
+  //   if (obstaclePosition <= 0) {
+  //     clearInterval(timerID);
+  //     alert('Game Over')
+  //   }
+
+  //   obstaclePosition -= 10;
+  //   obstacle.style.left = obstaclePosition + 'px'
+  // }, 20)
+  
+  setInterval(generateObstacles, 2000)
+  //setTimeout(generateObstacles, randomTime)
+
+    const obstacles = document.querySelectorAll('.obstacle');
+    obstacles.forEach(function(obstacle) {
+      alert(obstacle.style.right);
+      obstacle.style.right += obstacle.style.right + 'px';
+      if (obstacle.getBoundingClientRect().left < walkingBall.getBoundingClientRect().right) {
+        clearInterval(timerID);
+      }
+
+    //Check for collision 
+    const obstacleRect = obstacle.getBoundingClientRect();
+    const ballRect = walkingBall.getBoundingClientRect();
+    if (
+      obstacleRect.left < ballRect.right &&
+      obstacleRect.right > ballRect.left &&
+      obstacleRect.top < ballRect.bottom &&
+      obstacleRect.bottom > ballRect.top
+    ) {
+      clearInterval(timerID);
+      clearInterval(obstacleIntervalID);
+      alert('Game Over');
+    }
+
+  //   if (obstaclePosition <= 0) {
+  //     clearInterval(timerID);
+  //     obstacle.remove();
+  //   }
+  // }, 20);
+
+
+
 });
+})
